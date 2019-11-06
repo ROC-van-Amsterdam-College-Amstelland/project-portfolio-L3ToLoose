46efb6746b9c4126ace88f89ec3401bcc49bfc80
Yaiesj nazarali <yaiesj04@hotmail.com>
2019-11-06 11:46:19 +0100

    like of dislike button

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,8 @@
 var strNaam = prompt("voorbeeld van een javascript.vul uw naam in")
             alert("jouw naam is " + strNaam) ;
+
+            var clicks = 0;
+            function clickME() {
+                clicks += 1;
+                document.getElementById("clicks").innerHTML = clicks;
+         }
